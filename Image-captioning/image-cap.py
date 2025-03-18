7009464078d0b0b07f4bc952c5f6f9d91a1d510c
Rishab Paul <99572422+Addi2004@users.noreply.github.com>
2025-03-18 09:58:23 +0000

    Code

---
+++ Image-captioning/image-cap.py
@@ -0,0 +1,20 @@
+import requests
+from PIL import Image
+from transformers import AutoProcessor,BlipForConditionalGeneration 
+
+processor=AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model=BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+img_path="image.jpeg"
+image=Image.open(img_path).convert('RGB')
+
+text="The image of"
+inputs=processor(images=image,text=text,return_tensors='pt')
+
+#print(inputs)
+
+output=model.generate(**inputs)
+print(output)
+
+caption=processor.decode(output[0],skip_special_tokens='True')
+print(caption)
